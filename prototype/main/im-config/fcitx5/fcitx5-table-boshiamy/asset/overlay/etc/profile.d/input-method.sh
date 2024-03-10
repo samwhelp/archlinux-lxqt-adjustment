35a3e6fd08f2ceaad3509e6600fead36e0df662b
samwhelp <samwhelp@users.noreply.github.com>
2024-03-10 19:07:27 +0800

    fix

--- prototype/main/im-config/fcitx5/fcitx5-table-boshiamy/asset/overlay/etc/profile.d/input-method.sh
+++ prototype/main/im-config/fcitx5/fcitx5-table-boshiamy/asset/overlay/etc/profile.d/input-method.sh
@@ -9,9 +9,9 @@
 ##
 
 
-GTK_IM_MODULE=fcitx
-QT_IM_MODULE=fcitx
-XMODIFIERS=@im=fcitx
-SDL_IM_MODULE=fcitx
-INPUT_METHOD=fcitx
-GLFW_IM_MODULE=ibus
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+export SDL_IM_MODULE=fcitx
+export INPUT_METHOD=fcitx
+export GLFW_IM_MODULE=ibus
